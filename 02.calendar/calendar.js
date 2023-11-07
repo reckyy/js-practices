f0db1447e10b805a32e1d1509dd161eed92909fa
reckyy <srreki98@gmail.com>
2023-11-07 22:32:33 +0900

    💯 test: prettier

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,5 +1,5 @@
-import { DateTime } from 'luxon';
-import minimist from 'minimist';
+import { DateTime } from "luxon";
+import minimist from "minimist";
 
 const argv = minimist(process.argv.slice(2));
 const today = DateTime.now();
@@ -10,24 +10,24 @@ const entered_month = parseInt(argv.m) || today.month;
 
 /*日付を取得*/
 const dt = DateTime.fromObject({
-    year: entered_year,
-    month: entered_month
+  year: entered_year,
+  month: entered_month,
 });
 
 /*calendarを表示*/
 console.log("      %i月 %i\n日 月 火 水 木 金 土\n", dt.month, dt.year);
-const month_beginning_number = dt.startOf('month').weekday;
-if(month_beginning_number !== 7){
-  process.stdout.write('   '.repeat(month_beginning_number));
+const month_beginning_number = dt.startOf("month").weekday;
+if (month_beginning_number !== 7) {
+  process.stdout.write("   ".repeat(month_beginning_number));
 }
-for(let i=1; i <= dt.daysInMonth; i++){
-  const day = dt.set({day: i});
-  let day_string = String(i).padStart(2, ' ');
-  if(day.weekday === 6){
-    day_string += '\n';
-  }else{
-    day_string += ' ';
+for (let i = 1; i <= dt.daysInMonth; i++) {
+  const day = dt.set({ day: i });
+  let day_string = String(i).padStart(2, " ");
+  if (day.weekday === 6) {
+    day_string += "\n";
+  } else {
+    day_string += " ";
   }
   process.stdout.write(day_string);
 }
-console.log('');
+console.log("");
