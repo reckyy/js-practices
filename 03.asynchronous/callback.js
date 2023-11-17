0c86377b5fdc4540eda0865f44756d9b5d3e1845
reckyy <srreki98@gmail.com>
2023-11-17 22:00:10 +0900

    🐛 fix: エラーシナリオ統一

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -21,7 +21,7 @@ console.log("エラーあり");
 db.run(
   "create table books(id integer primary key autoincrement, title text unique)",
   () => {
-    db.run("insert into users(title)", function (err) {
+    db.run("insert into users(title) values(?)", "テスト", function (err) {
       if (err) {
         console.log(err);
       } else {
