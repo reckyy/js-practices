4b34a2da845d7193fc8bb6b2318d48ba112afb98
reckyy <srreki98@gmail.com>
2023-11-29 21:08:53 +0900

    ⚡️ feat: 一旦メモの追加、一覧、詳細表示機能追加

--- 04.class/class.js
+++ 04.class/class.js
@@ -1,9 +1,8 @@
 import sqlite3 from "sqlite3";
 import readline from "readline";
+import enquirer from "enquirer";
 
-const dbPath = "memo.db";
-
-const db = new sqlite3.Database(dbPath);
+const db = new sqlite3.Database("./memo.sqlite");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -19,7 +18,7 @@ const createTable = () => {
         } else {
           resolve();
         }
-      },
+      }
     );
   });
 };
@@ -30,25 +29,79 @@ const addMemo = (data) => {
       if (err) {
         reject(err);
       } else {
-        resolve();  
+        resolve();
       }
     });
   });
 };
 
-const lines = [];
-rl.on("line", (line) => {
-  lines.push(line);
-});
+const getAllMemo = () => {
+  return new Promise((resolve, reject) => {
+    db.all("select * from memos", function (err, rows) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+};
 
-rl.on("close", async () => {
-  const input = lines.join('\n');
-  await createTable();
-  try {
-    await addMemo(input);
-  } catch (e) {
-    console.error(e);
-  } finally {
-    db.close();
-  }
-});
+const option = process.argv[2];
+
+
+if (option) {
+  (async () => {
+    const memos = await getAllMemo();
+		const firstRowsOfMemos = memos.map(({id, content}) => ({
+            id: id,
+            value: content.split('\n')[0],
+          }));
+    try {
+      switch (option) {
+        case '-r': {
+					const questions = [
+					  {
+					    type: "select",
+					    name: "chosenMemoId",
+					    message: "Choose a note you want to see:",
+					    choices: firstRowsOfMemos,
+					    result() {
+					      return this.focused.id;
+					    },
+					  },
+					];
+          const answer = await enquirer.prompt(questions);
+          console.log(memos[answer.chosenMemoId - 1].content);
+          break;
+				}
+        case '-l': {
+					firstRowsOfMemos.forEach((row) => console.log(row.value));
+          break;
+				}
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      db.close();
+      rl.close();
+    }
+  })();
+} else {
+  const lines = [];
+  rl.on("line", (line) => {
+    lines.push(line);
+  });
+
+  rl.on("close", async () => {
+    const input = lines.join("\n");
+    await createTable();
+    try {
+      await addMemo(input);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      db.close();
+    }
+  });
+}
