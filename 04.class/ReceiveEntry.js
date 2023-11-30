7a8e54d34c1694f9d2b4ae331fb653c06a6ed5b1
reckyy <srreki98@gmail.com>
2023-11-30 14:50:10 +0900

    🐛 fix: 入力保存の処理をsaveInputメソッドとして、ReceiveEntryクラスに定義

--- 04.class/ReceiveEntry.js
+++ 04.class/ReceiveEntry.js
@@ -6,4 +6,24 @@ export class ReceiveEntry{
       input: process.stdin,
     });
   }
+
+  saveInput(sql){
+    const lines = [];
+    this.rl.on("line", (line) => {
+      lines.push(line);
+    })
+    
+    this.rl.on("close", async() => {
+      const input = lines.join('\n');
+      await sql.run("create table if not exists memos (id integer primary key autoincrement, content text)");
+      try {
+        await sql.run(`insert into memos(content) values('${input}')`);
+        console.log('your entry is saved!');
+      } catch (err) {
+        console.error(err);
+      } finally {
+        sql.close();
+      }
+    });
+  }
 }
