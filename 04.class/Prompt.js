0dbaf2ce3d4c0bb5472d84693d3f47a173b40f98
reckyy <srreki98@gmail.com>
2023-12-01 09:40:30 +0900

    ⚡️ feat: 一覧機能をPrompt.jsに移動

--- 04.class/Prompt.js
+++ 04.class/Prompt.js
@@ -4,4 +4,27 @@ export class Prompt{
   constructor(){
     this.option = process.argv[2];
   }
+
+
+  pattern_by_prompt(sql){
+    (async () => {
+      const memos = await sql.all();
+      const firstRowsOfMemos = memos.map(({id, content}) => ({
+        id: id,
+        value: content.split('\n')[0],
+      }));
+      try{
+        switch(this.option){
+          case '-l': {
+            firstRowsOfMemos.forEach((row) => console.log(row.value));
+            break;
+          }
+        }
+      }catch(err){
+        console.error(err);
+      }finally{
+        sql.close();
+      }
+    })();
+  }
 }
