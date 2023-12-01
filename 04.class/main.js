b571527ec526645306176fbc9a011c4a02e32bed
reckyy <srreki98@gmail.com>
2023-12-01 12:04:59 +0900

    ⚡️ feat: Main.jsからmain.jsに変更

---
+++ 04.class/main.js
@@ -0,0 +1,12 @@
+import { Sqlite } from "./Sqlite.js";
+import { ReceiveEntry } from "./ReceiveEntry.js";
+import { Prompt } from "./Prompt.js";
+
+const sql = new Sqlite();
+const prompt = new Prompt();
+
+if(prompt.option){
+  prompt.pattern_by_prompt(sql);
+}else{
+  ReceiveEntry.saveInput(sql);
+}
