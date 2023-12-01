c9d708d708a3cb28cd0b772fc3f79904f90b2e14
reckyy <srreki98@gmail.com>
2023-12-01 15:46:22 +0900

    🐛 fix: 即時実行ではなく、top-level awaitを用いて記述

--- 03.asynchronous/async.js
+++ 03.asynchronous/async.js
@@ -5,37 +5,33 @@ import { run, get } from "./mySqlite.js";
 const db = new sqlite3.Database(":memory:");
 
 console.log("エラーなし");
-(async () => {
-  await run(
-    db,
-    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE)"
-  );
-  const result = await run(db, "INSERT INTO books(title) VALUES(?)", "テスト");
-  console.log(`自動採番ID :${result.lastID}`);
-  const row = await get(db, "SELECT * FROM books WHERE id = ?", result.lastID);
-  console.log(`id:${row.id} タイトル:${row.title}`);
-  await run(db, "DROP TABLE books");
-})();
+await run(
+  db,
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE)"
+);
+const result = await run(db, "INSERT INTO books(title) VALUES(?)", "テスト");
+console.log(`自動採番ID :${result.lastID}`);
+const row = await get(db, "SELECT * FROM books WHERE id = ?", result.lastID);
+console.log(`id:${row.id} タイトル:${row.title}`);
+await run(db, "DROP TABLE books");
 
 await timers.setTimeout(100);
 
 console.log("エラーあり");
-(async () => {
-  await run(
-    db,
-    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE)"
-  );
-  try {
-    await run(db, "INSERT INTO users(title) VALUES(?)", "テスト");
-  } catch (err) {
-    console.error(err.message);
-  }
-  try {
-    await get(db, "SELECT * FROM users WHERE id = ?", 1);
-  } catch (err) {
-    console.error(err.message);
-  } finally {
-    await run(db, "DROP TABLE books");
-    db.close();
-  }
-})();
+await run(
+  db,
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE)"
+);
+try {
+  await run(db, "INSERT INTO users(title) VALUES(?)", "テスト");
+} catch (err) {
+  console.error(err.message);
+}
+try {
+  await get(db, "SELECT * FROM users WHERE id = ?", 1);
+} catch (err) {
+  console.error(err.message);
+} finally {
+  await run(db, "DROP TABLE books");
+  db.close();
+}
