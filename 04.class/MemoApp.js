5e059e482c075a97e6c95834b71971342d059c5e
reckyy <srreki98@gmail.com>
2023-12-13 15:26:51 +0900

    🍀 refactor: オプションの有無による処理をrunメソッドに統合

--- 04.class/MemoApp.js
+++ 04.class/MemoApp.js
@@ -7,83 +7,83 @@ export class MemoApp {
   }
 
   run(sql) {
-    (async () => {
-      const memos = await sql.all();
-      const firstRowsOfMemos = memos.map(({ id, content }) => ({
-        id: id,
-        value: content.split("\n")[0],
-      }));
-      try {
-        switch (this.option) {
-          case "-l": {
-            firstRowsOfMemos.forEach((row) => console.log(row.value));
-            break;
-          }
-          case "-r": {
-            const questions = [
-              {
-                type: "select",
-                name: "chosenMemoId",
-                message: "Choose a note you want to see:",
-                choices: firstRowsOfMemos,
-                result() {
-                  return this.focused.id;
+    if (this.option){
+      (async () => {
+        const memos = await sql.all();
+        const firstRowsOfMemos = memos.map(({ id, content }) => ({
+          id: id,
+          value: content.split("\n")[0],
+        }));
+        try {
+          switch (this.option) {
+            case "-l": {
+              firstRowsOfMemos.forEach((row) => console.log(row.value));
+              break;
+            }
+            case "-r": {
+              const questions = [
+                {
+                  type: "select",
+                  name: "chosenMemoId",
+                  message: "Choose a note you want to see:",
+                  choices: firstRowsOfMemos,
+                  result() {
+                    return this.focused.id;
+                  },
                 },
-              },
-            ];
-            const answer = await enquirer.prompt(questions);
-            const chosenMemo = memos.find(
-              (memo) => memo.id === answer.chosenMemoId
-            );
-            console.log(chosenMemo.content);
-            break;
-          }
-          case "-d": {
-            const questions = [
-              {
-                type: "select",
-                name: "chosenMemoId",
-                message: "Choose a note you want to delete:",
-                choices: firstRowsOfMemos,
-                result() {
-                  return this.focused.id;
+              ];
+              const answer = await enquirer.prompt(questions);
+              const chosenMemo = memos.find(
+                (memo) => memo.id === answer.chosenMemoId
+              );
+              console.log(chosenMemo.content);
+              break;
+            }
+            case "-d": {
+              const questions = [
+                {
+                  type: "select",
+                  name: "chosenMemoId",
+                  message: "Choose a note you want to delete:",
+                  choices: firstRowsOfMemos,
+                  result() {
+                    return this.focused.id;
+                  },
                 },
-              },
-            ];
-            const answer = await enquirer.prompt(questions);
-            await sql.delete(answer.chosenMemoId);
-            console.log("memo of your choice is deleted!");
-            break;
+              ];
+              const answer = await enquirer.prompt(questions);
+              await sql.delete(answer.chosenMemoId);
+              console.log("memo of your choice is deleted!");
+              break;
+            }
           }
+        } catch (err) {
+          console.error(err);
+        } finally {
+          sql.close();
         }
-      } catch (err) {
-        console.error(err);
-      } finally {
-        sql.close();
-      }
-    })();
-  }
+      })();
+    } else {
+      const rl = readline.createInterface({
+        input: process.stdin,
+      });
+      const lines = [];
+      rl.on("line", (line) => {
+        lines.push(line);
+      });
 
-  saveInput(sql) {
-    const rl = readline.createInterface({
-      input: process.stdin,
-    });
-    const lines = [];
-    rl.on("line", (line) => {
-      lines.push(line);
-    });
-
-    rl.on("close", async () => {
-      const input = lines.join("\n");
-      await sql.createTable();
-      try {
-        await sql.add(input);
-        console.log("your entry is saved!");
-      } catch (err) {
-        console.error(err);
-      } finally {
-        sql.close();
-      }
-    });
+      rl.on("close", async () => {
+        const input = lines.join("\n");
+        await sql.createTable();
+        try {
+          await sql.add(input);
+          console.log("your entry is saved!");
+        } catch (err) {
+          console.error(err);
+        } finally {
+          sql.close();
+        }
+      });
+    }
   }
 }
--- 04.class/main.js
+++ 04.class/main.js
@@ -4,8 +4,4 @@ import { MemoApp } from "./MemoApp.js";
 const sql = new MemoStorage();
 const memoApp = new MemoApp();
 
-if (memoApp.option) {
-  memoApp.run(sql);
-} else {
-  memoApp.saveInput(sql);
-}
+memoApp.run(sql);
