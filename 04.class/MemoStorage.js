c49288272cb7394bcd67b0c4ae05b020db61236f
reckyy <srreki98@gmail.com>
2023-12-16 22:16:18 +0900

    📃 chore: all()のコールバック関数を他と同じfunctionに修正

--- 04.class/MemoStorage.js
+++ 04.class/MemoStorage.js
@@ -38,7 +38,7 @@ export class MemoStorage {
 
   all() {
     return new Promise((resolve, reject) => {
-      this.db.all("SELECT * FROM memos ORDER BY id", (err, rows) => {
+      this.db.all("SELECT * FROM memos ORDER BY id", function (err, rows) {
         if (err) {
           reject(err);
         } else {
